cafabb8a83dba6378ba248729c77e103fa084422
lagneshmitraoffice-rgb <lagneshmitraoffice@gmail.com>
2026-02-16 02:30:53 +0530

    Update Firebase config with new keys and IDs
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,15 @@
+// Firebase CDN v12 ONLY
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "PASTE_NEW_KEY",
+  authDomain: "PASTE_PROJECT.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
