5f2868d9c950cf52ddcee0bd34da96f1d12c9a91
lagneshmitraoffice-rgb <lagneshmitraoffice@gmail.com>
2026-02-16 09:36:06 +0530

    Update Firebase SDK version and configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+// Firebase SDK imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// Firebase config
 const firebaseConfig = {
-  apiKey: "PASTE_REAL_API_KEY",
-  authDomain: "PASTE_REAL.firebaseapp.com",
-  projectId: "PASTE_REAL",
-  storageBucket: "PASTE_REAL.appspot.com",
-  messagingSenderId: "PASTE_REAL",
-  appId: "PASTE_REAL"
+  apiKey: "AIzaSyDxefsDzhoQyDv2X339PBEqy2o1q2hYf7U",
+  authDomain: "lagneshmitra-98176.firebaseapp.com",
+  projectId: "lagneshmitra-98176",
+  storageBucket: "lagneshmitra-98176.firebasestorage.app",
+  messagingSenderId: "1046880553468",
+  appId: "1:1046880553468:web:1f23caef22f08b84304a79",
+  measurementId: "G-LC3FSBJWJT"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
+// Init firebase
